9a759e65be3900adf1e92778a8441d285b89b4a5
MoheenSajjad <moheensajjad82@gmail.com>
2023-09-17 15:10:30 +0500

    final project updated

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,12 +3,7 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [
-  {
-    username:"Moheen Sajjad",
-    password:123456
-  }
-];
+let users = [];
 
 
 const isValid = (username)=>{ //returns boolean
